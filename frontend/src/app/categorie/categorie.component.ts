c27f313e0c4a052a53eb216ba8ed357c7c7b7124
ali boulajine <abdelaliboulajine@gmail.com>
2019-04-08 06:53:53 +0200

    deploy

--- frontend/src/app/categorie/categorie.component.ts
+++ frontend/src/app/categorie/categorie.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { CategorieService } from '../core/categorie.service';
-import { Categorie } from '../model/Categorie.model';
+import { Categorie } from '../model/categorie.model';
 
 @Component({
   selector: 'app-categorie',
